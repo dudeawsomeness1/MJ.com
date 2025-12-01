233a3468235b4248fb4a39c4646b17a1e8c16a42
Jelly Bean <13771643+dudeawsomeness1@users.noreply.github.com>
2025-12-01 00:52:06 -0700

    Quartz sync: Dec 1, 2025, 12:52 AM

--- content/Art/Photography/Le Moon.md
+++ content/Art/Photography/Le Moon.md
@@ -1,3 +1,19 @@
 Ah, le moon…
 
+## Le Crescent
+
 ![[DSCN2157.jpg]]
+
+## Le ½
+
+![[FSCN1290.jpg]]
+
+## Le Whole
+
+![[DSCN1078.jpg]]
+
+![[DSCN1076.jpg]]
+
+## Le Halo
+
+![[IMG_9237.heic]]
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({folderDefaultState: "open"}),
   ],
   right: [
     Component.Graph(),
@@ -63,7 +63,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({folderDefaultState: "open"}),
   ],
   right: [],
 }
